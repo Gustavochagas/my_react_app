32a51c648012c8033d5d5dc0ab018dc7f0c0448c
Gustavo Chagas <gustavochagasb@gmail.com>
2019-01-10 11:07:57 -0200

    Install react router

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            {/* <Route exact path="/" component={ }/>
+            <Route path="/products/:id" component={ }/> */}
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
